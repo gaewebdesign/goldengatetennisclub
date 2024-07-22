b74d89199c6a1d2f2f6d84b68ee1753c8c7bb9e8
githubmutt@gmail.com <github.mutt@gmail.com>
2024-07-22 13:30:38 -0700

    tournament

---
+++ imports_/private.php
@@ -0,0 +1,79 @@
+<html>
+
+<head>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<style>
+ .navbar-brand{
+   font-size: smaller;
+ }
+
+ .dropdown-item{
+
+#
+#    background-color: #fff; /* here */
+#    background: #fff;
+     background-color: rgb(202,233,245);
+     background: rgb(202,233,245);    
+ }
+
+   .dropdown{
+     color: blue;
+      font-size: smaller;
+
+   }
+
+   .colorfill{
+    background: rgb(202,233,245);
+  }
+      
+
+</style>
+
+</head>
+<!--
+
+https://getbootstrap.com/docs/5.1/getting-started/introduction/
+
+-->
+
+
+
+<nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="./index.html">Home</a> 
+      <a class="navbar-brand" href="./email.html">Members</a>
+      <a class="navbar-brand" href="./email_tournament.html">Tournament</a>
+
+
+      <div class="dropdown" >
+          <a class="dropdown-toggle"  id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+            Tournament
+          </a>
+        <ul class="dropdown-menu dropdown-menu-dark colorfill" aria-labelledby="dropdownMenuButton2">
+              <li><a class="dropdown-item active" href="./tournament.html?draw=0">Info</a></li>
+              <li><a class="dropdown-item dropdown-menu-dark colorfill" disabled href="./tournament.html?draw=1">Players</a></li>
+              <li><a class="dropdown-item" href="./tournament.html?draw=2">Draws (M 3.5)</a></li>
+              <li><a class="dropdown-item" href="./tournament.html?draw=3">Draws (M 4.0)</a></li>
+              <li><hr class="dropdown-divider" style="border-color:black;"></li>
+              <li><a class="dropdown-item disabled"  href="./tournament.html?draw=4">Enter Tournament</a></li>
+        </ul>
+        </div>
+
+ </p>
+      
+</body>
+</html>
+
+<!--
+ Previous without Drop-Down
+<a href="./index.phtml"> Home </a> |   
+<a href="./email.phtml" >Email </a> |
+<a href="./email_free.phtml"> Pigout </a> |
+<a href="./email_lunch.phtml"> Fatima Lunch </a> |
+<a href="./email_join.phtml" >Athena Dinner</a>
+
+</a>
+-->
---
+++ tournament_/mens.php
@@ -0,0 +1,147 @@
+<html>
+
+   <div class="h3">2024 M 3.5 Tournament</div>
+
+ <main id="headers">
+
+ <ul class="round">
+   Quarter-Finals
+   <div class="small">Finish by 9/7/2024</div>
+  </ul>
+  <ul class="round">
+   Semi-Finals
+   <div class="small">Finish by 9/21/2024</div></ul>
+  <ul class="round">
+   Finals
+   <div class="small">Finish by 10/4/2024</div>
+  </ul>
+  
+</main>
+
+<body>
+<main id="tournament2">
+<?php
+//$q = array( "Carlos Alcarez", "Tommy Paul", "Daniell Medvedev","Jannik Sinner");
+
+$q[] = "Carlos Alcarez";
+$q[] = "Tommy Paul";
+$q[] = "Danill Medvedev";
+$q[]= "Jannik Sinner";
+$q[] = "Lorenzo Mussetti";
+$q[] = "Taylor Fritz";
+$q[] = "Alex de Minaur";
+$q[] = "Novak Djokovic";
+
+$s1 = "7-6 ,6-1";
+$s2 = "6-1 ,6-4";
+$s3 = "6-3 ,6-4";
+$s4 = "6-4 ,4-6,6-2";
+
+$f1 = "6-4 ,7-6";
+$f2 = "6-3 ,4-0 (ret)";
+
+$w1 = "6-2,6-2";
+
+$q1 = "Dimitrov/Shelton";
+$q2 = "Ru/Paul";
+$q3 = "Struff/Borges";
+$q4 = "Sinner/Medvedev";
+$q5 = "Machac/Navone";
+$q6 = "Fritz/Popyrin";
+$q7 = "Arnaldi/Thompson";
+$q8 = "Jarry/Griekspoor";
+
+
+
+
+$s1 = $q1;  $_s1="6-2,6-1";
+$s2 = $q3;  $_s2 ="6-2,6-1";
+$s3 = $q5;  $_s3="6-2,6-1";
+$s4 = $q8;  $_s4 = "6-2,6-1";
+
+
+$f1 = $s1; $_f1 = "7-6,7-5";
+$f2 = $s3; $_f2 = "7-6,5-7,1-0";
+
+$w = $f2;  $_w= "7-6,7-5";
+
+
+$q1 = $q2 = $q3 = $q4 = $q5 = $q6 = $q7 = $q8 = ".";
+$s1 = $s2 = $s3 = $s4 = ".";
+$f1 = $f2 = "";
+
+$_s1 = $_s2 = $_s3 = $_s4 = "";
+$_f1 = $_f2 = "";
+$_w = "";
+/*
+
+
+
+*/
+?>
+<ul class="round">
+  <li class="spacer">
+  <li class="game-top"> <?php echo $q1 ?> <span></span> </li>
+  <li class="draw"></li>
+  <li class="game-top"> <?php echo $q2 ?> <span></span> </li>
+  <li class="spacer">  
+
+<!--   -->
+  <li class="game-top"> <?php echo $q3 ?> <span></span> </li>
+  <li class="draw"></li>
+  <li class="game-top"><?php echo $q4 ?><span></span> </li>
+
+  <li class="spacer"> 
+<!--   -->
+
+<li class="game-top"><?php echo $q5 ?> <span></span> </li>
+  <li class="draw"></li>
+  <li class="game-top"><?php echo $q6 ?> <span></span> </li>
+
+  <li class="spacer"> 
+<!--   -->
+<li class="game-top"> <?php echo $q7 ?> <span></span> </li>
+  <li class="draw"></li>
+  <li class="game-top"><?php echo $q8 ?>  <span></span> </li>
+
+  <li class="spacer"> 
+<!--   -->
+
+
+
+</ul>
+
+<!-- Semi-Finals  -->
+<ul class="round">
+<li class="game-top"> <?php echo $s1 ?>  <span class="small"><?php echo $_s1 ?></span> </li>
+  <li class="draw draw2"></li>
+  <li class="game-top">  <?php echo $s2 ?>   <span class="small"><?php echo $_s2 ?></span> </li>
+  <li class="spacer"> 
+  <li class="spacer"> 
+<li class="game-top">  <?php echo $s3 ?>   <span class="small"><?php echo $_s3 ?></span> </li>
+  <li class="draw draw2"></li>
+  <li class="game-top">  <?php echo $s4 ?>   <span class="small"><?php echo $_s4 ?></span> </li>
+</ul>
+
+ <!-- Finals -->
+  <ul class="round">
+  <li class="game-top">  <?php echo $f1 ?>   <span class="small"><?php echo $_f1 ?></span </li>
+  <li class="draw draw3"></li>
+  <li class="game-top">  <?php echo $f2 ?>   <span class="small"><?php echo $_f2 ?></span </li>
+
+</ul>
+
+ <!-- Winner -->
+<ul class="round">
+
+<li class="game-top">  <?php echo $f2 ?>   <span class="small"><?php echo $_w ?></span> </li>
+</ul>
+
+</main>
+
+
+
+<body>
+      
+</body>
+</html>
