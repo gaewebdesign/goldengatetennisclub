b0ee0e438137d6cd20a1af304984cc43cb47591a
githubmutt@gmail.com <github.mutt@gmail.com>
2026-01-30 23:51:31 -0800

    members

---
+++ archive.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <link rel="stylesheet" href="./css/index.css" >
+
+      <title>Golden Gate Tennis Club</title>
+
+
+</head>
+<body class="Back">
+
+</p>
+<div class="container-sm ">
+      <div class="row">
+      <div class="col-sm-1">  </div>            
+       <div class="col-sm-10 ">
+             <center>
+                  <?php include "imports_/banner.php"       ?>
+            </center> 
+      </div>
+      <div class="col-sm-1">
+      </div>
+      </div>  
+      
+      </div>
+
+<div class="container-sm Title">
+    <div class="row">
+      
+        <div class="col-sm-12"> 
+            <?php include "./imports_/ggtc.php" ?>
+        </div>
+    </div>
+
+
+</div>
+
+<div class="container-sm ">
+      <div class="row">
+            <div class="col-sm-2"> </div> 
+            <div class="col-sm-8 NavLink"> 
+ 
+                   <?php include "./imports_/navlink.php" ?>
+            </div>
+
+
+      <div class="col-sm-2"> </div> 
+      </div>
+  
+  
+  </div>
+
+  <div class="container-sm ">
+      <div class="row">
+
+            <div class="col-sm-2 text-center"></div>
+            <div class="col-sm-8 text-center">
+
+             <p>&nbsp;<br>
+            <img class="ResponsiveImage" src="./images/alicemarble.jpg">
+
+             <p>&nbsp;<br>
+             <hr/>
+             
+             <strong>Alice Marble</strong> (September 28, 1913 &ndash; December 13, 1990) 
+            was an American tennis player who won 18 Grand Slam championships from 1936 through 1940. 
+            Five of those championships were in singles, six were in women's doubles, and seven were in mixed doubles.
+             Born in the small town of Beckwourth, California, Marble moved with her family at the age of five to San Francisco.
+             She excelled in many sports such as baseball, but her brother persuaded her to try a more lady-like sport, tennis,
+              which she quickly honed while playing in Golden Gate Park. 
+              As teen, she  won several California junior tournaments.
+               She was challenged in 1934 after collapsing during a match at the French Championships.
+               Doctors diagnosed her with pleurisy and tuberculosis. She took an extended rest but roared back into competition. 
+               At the U.S. Championships, Marble won the singles title in 1936, 1938, 1939, and 1940. 
+               She won the women's doubles title with Sarah Palfrey Cooke in 1937, 1938, 1939, and 1940. 
+               And she won the mixed doubles title in 1936 (with Gene Mako), 1938 (with Don Budge), 1939 (with Harry Hopman),
+                and 1940 (with Bobby Riggs). At Wimbledon, Marble won the singles title in 1939.
+                 She won the women's doubles title with Cooke in 1938 and 1939. 
+                 And she won the mixed doubles title in 1937 (with Budge), 1938 (with Budge), and 1939 (with Riggs).
+                  In Wightman Cup competition, Marble lost only one singles and one doubles match during the years she 
+                  competed (1933 and 1937 through 1940). Marble was the Associated Press Athlete of the Year in 1939 and 1940. 
+                  After capping a stellar amateur career in 1940, Marble turned professional and earned a great sum of money traveling 
+                  around playing exhibition tournaments. Through illness and victory, Marble had the support of her coach and mentor, 
+                  Eleanor "Teach" Tennant, who changed her technique and influenced her life. During World War II,
+                   Marble was married to Joe Crowley, a pilot, who was killed in action over Germany. Only days before his death,
+                    she miscarried their child following a car accident. After an attempt to kill herself, she recuperated and in 1945 agreed to spy for U.S.
+                     intelligence. Her mission involved renewing contact with a former lover, a Swiss banker, and obtaining Nazi financial data. 
+                     The operation ended when a Nazi agent shot her in the back. She was extracted and recovered from her injury. 
+                     Few details of the operation ever emerged because of her silence. 
+                     The story was told only after her death when Courting Danger (ISBN 0-312-92813-0), 
+                     a second autobiography, was published. In 1964, Marble was inducted into the International Tennis Hall of Fame.
+                      Weakened by pernicious anemia, Marble died at a hospital in Palm Springs, California.<br /><br />
+
+                  </div>
+
+                      <div class="col-sm-2 text-center"></div>
+
+
+
+             </p>
+
+
+
+
+            
+            <p>&nbsp;
+      </div> 
+      </div>
+  
+  
+  </div>
+  
+
+
+<!--
+  <div class="container">
+      <div class="row ">
+        <div class="col-sm-1 ">
+        </div>
+        <div class="col-sm-10">
+
+ 
+
+      </div>
+
+        </div>
+
+        <div class="col-sm-1"></div>
+      </div>
+-->
+      <div class="container-sm ">
+            <div class="row">
+                  <div class="col-sm-2"> &nbsp;</div>                   
+                  <div class="col-sm-8"> &nbsp;
+
+                        <?php include "./imports_/footer.php" ?>
+
+                  </div> 
+                  <div class="col-sm-2"> &nbsp;</div> 
+            </div>
+        
+        
+        </div>
+
+
+
+<body>
+      
+</body>
+</html>
